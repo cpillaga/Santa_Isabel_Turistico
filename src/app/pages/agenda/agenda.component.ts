d06e7382f1fddbd877aa50209ecb768bf7e5ed29
Christian Pillaga <christian_pillaga_s@hotmail.es>
2021-02-26 13:44:11 -0500

    add hora

--- src/app/pages/agenda/agenda.component.ts
+++ src/app/pages/agenda/agenda.component.ts
@@ -28,14 +28,18 @@ export class AgendaComponent implements OnInit {
   getAgenda(){
     this._agendaService.getAgenda().subscribe(resp => {
       this.agenda = resp;
+      // console.log(this.agenda);
     });
   }
 
   agregar(form: NgForm){
+    // 2021-09-24T00:00:00.000Z
+    const fecha = form.value.fecha + "T" + form.value.hora + ":00.000Z";
+
     const agenda = {
       titulo: form.value.titulo,
       descripcion: form.value.descripcion, 
-      fecha: new Date(form.value.fecha).toISOString(),
+      fecha: new Date(fecha).toISOString(),
       lugar: form.value.lugar
     }
 
